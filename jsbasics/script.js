0178037bc0bd370ce019f2f97dfd51a55198b28d
unknown <dsarmins@gmail.com>
2019-10-29 21:31:45 +0200

    javascript

---
+++ jsbasics/script.js
@@ -0,0 +1,57 @@
+console.log("hello world");
+
+var nosaukums = "mans nosaukums";
+
+console.log(nosaukums);
+
+console.log(typeof (nosaukums));
+
+nosaukums = 234;
+console.log(nosaukums);
+console.log(typeof (nosaukums))
+
+const skolasNosaukums ="Riga coding school";
+
+
+const skolas_nosaukums ="Riga coding school";
+
+//skolasNosaukums ="RCS"
+
+console.log (skolasNosaukums)
+
+var a;
+console.log(a);
+
+a = 7;
+var b = a;
+
+
+c = 5;
+//alert(c);
+
+var q = 1;
+var w = 2;
+var e ="Im string"
+
+q = q + w;
+console.log(q);
+
+w = w + w;
+console.log(w);
+
+w = 1 + w;
+console.log(w);
+
+var z = "6"
+console.log(z+1);
+console.log(z-1);
+
+var d = q * w;
+console.log(d);
+
+var g = 1 / 2;
+console.log(g);
+
+console.log(typeof (z))
+
+console.log(33 % 4)
